a0a16c581b306642e53c85a2b46693a31a5456ab
arthur <arthur.guerinault@epitech.eu>
2025-06-13 23:27:38 +0200

    feat: first captcha (odd tester where you can't do odd)

---
+++ captchaware/src/app/sliderCaptcha/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const OddNumberComponent = ({ number }: { number: number }) => {
+  const [waitTime, setWaitTime] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setWaitTime(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <p>{waitTime ? number + 1 : number}</p>
+      <p>
+        {waitTime
+          ? "Oops, randomization had some delay. Try again !"
+          : "Well done! You are a human."}
+      </p>
+    </div>
+  );
+};
+
+export default function OddCaptcha() {
+  const [luckNumber, setLuckNumber] = useState(0);
+
+  const handleClickLuck = () => {
+    const randomNumber = Math.floor(Math.random() * 100);
+    setLuckNumber(randomNumber);
+  };
+
+  return (
+    <div className="space-y-8">
+      <p>You need to do odd to verify if you are human.</p>
+      <div className="border-2 border-solid border-blue-200 w-fit">
+        <button onClick={handleClickLuck}>Random</button>
+      </div>
+      <div>
+        {luckNumber % 2 === 0 ? (
+          <p>{luckNumber}</p>
+        ) : (
+          <OddNumberComponent number={luckNumber} />
+        )}
+      </div>
+    </div>
+  );
+}
+
