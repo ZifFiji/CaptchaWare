af3ba8cba15207fa728db5dfabdfbac9c7f5611e
etienne <etienne.labarbe@epitech.eu>
2025-06-15 13:11:35 +0200

    feat: chess solver

---
+++ captchaware/src/app/chess/page.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+const ChessCaptchaComponent = ({isCorrect}: {isCorrect: boolean}) => {
+  return (
+    <div>
+      <p>
+        {isCorrect && "Correct."}
+        {!isCorrect && "Incorrect. Try again!"}
+      </p>
+    </div>
+  )
+}
+
+export default function ChessCaptcha() {
+  const [response, setResponse] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const result = "Rc6+";
+
+  const handleClick = () => {
+    setSubmitted(true);
+    setIsCorrect(response.trim() === result);
+  }
+
+  return (
+    <div className="mb-4 flex h-screen flex-col items-center justify-center">
+      <p className="mb-4">White to move and NOT checkmate in 1</p>
+      <div className="mb-4 flex w-fit items-center justify-center border-2 border-blue-200 border-solid p-4">
+        <Image
+          src="/chess.jpg"
+          alt="Chessboard"
+          width={400}
+          height={400}
+          className="rounded-lg"
+        />
+      </div>
+      <div className="mb-4 flex flex-col items-center justify-center space-y-4">
+        <input
+          type="text"
+          value={response}
+          onChange={(e) => setResponse(e.target.value)}
+          placeholder="Enter your response"
+          className="mb-4 w-full max-w-md rounded border border-gray-300 px-3 py-2"
+        />
+        <button type="button" onClick={handleClick} className="cursor-pointer rounded-lg bg-zinc-800 px-8 py-4 font-bold hover:bg-zinc-700">Verify</button>
+      </div>
+      {submitted && (
+        <div>
+          <ChessCaptchaComponent isCorrect={isCorrect}/>
+        </div>
+      )}
+    </div>
+  );
+}
--- captchaware/src/app/page.tsx
+++ captchaware/src/app/page.tsx
@@ -25,6 +25,8 @@ export default function Home() {
         />
         <GameCard href="/guessCaptcha" title="Ninth Captcha" color="bg-gradient-to-r from-lime-500 to-lime-600 hover:from-lime-600 hover:to-lime-700" />
         <GameCard href="/wordTTS" title="Tenth Captcha" color="bg-gradient-to-r from-teal-500 to-teal-600 hover:from-teal-600 hover:to-teal-700" />
+        <GameCard href="/chess" title="Eleventh Captcha" color="bg-gradient-to-r from-cyan-500 to-cyan-600 hover:from-cyan-600 hover:to-cyan-700" />
+
       </div>
     </div>
   )
